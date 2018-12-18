d99720b700cc3ff2cf4ffd12fe52bb1d01a7a5dd
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-12-19 00:53:02 +0200

    new package: libstdc++

---
+++ packages/libstdc++/build.sh
@@ -0,0 +1,11 @@
+TERMUX_PKG_HOMEPAGE=http://gcc.gnu.org
+TERMUX_PKG_DESCRIPTION="The standard C++ library"
+TERMUX_PKG_VERSION=6.4.0
+TERMUX_PKG_DEPENDS="musl"
+
+TERMUX_PKG_RM_AFTER_INSTALL="lib/libstdc++.so.6.0.22-gdb.py"
+
+termux_step_make_install() {
+    cp -f "/opt/termux/${TERMUX_HOST_PLATFORM}/${TERMUX_HOST_PLATFORM}/lib/"libstdc++.so.* \
+        "${TERMUX_PREFIX}/lib/"
+}
