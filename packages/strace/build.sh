3e931cd14f975e1c4d9d1ae9c301f86743c779e3
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-12-20 16:21:26 +0200

    new package: strace
    Very useful in debugging 'Bad system call' errors.

---
+++ packages/strace/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE=https://strace.io/
+TERMUX_PKG_DESCRIPTION="Debugging utility to monitor system calls and signals received"
+TERMUX_PKG_VERSION=4.25
+TERMUX_PKG_SRCURL=https://github.com/strace/strace/releases/download/v$TERMUX_PKG_VERSION/strace-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=d685f8e65470b7832c3aff60c57ab4459f26ff89f07c10f92bd70ee89efac701
+
+TERMUX_PKG_RM_AFTER_INSTALL=bin/strace-graph # This is a perl script
+
+# Without st_cv_m32_mpers=no the build fails if gawk is installed.
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+st_cv_m32_mpers=no
+--enable-mpers=no
+--without-libdw
+"
