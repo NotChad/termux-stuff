a3efa7cef7567df980b4cadace3d400682d0a38a
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-12-22 16:03:30 +0200

    python: depend on musl

--- packages/python/build.sh
+++ packages/python/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://python.org/
 TERMUX_PKG_DESCRIPTION="Python 3 programming language intended to enable clear programs"
 _MAJOR_VERSION=3.6
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.6
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://www.python.org/ftp/python/${TERMUX_PKG_VERSION}/Python-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=d79bc15d456e73a3173a2938f18a17e5149c850ebdedf84a78067f501ee6e16f
-TERMUX_PKG_DEPENDS="gdbm, libbz2, libffi, liblzma, libsqlite, ncurses, ncurses-ui-libs, openssl, readline"
+TERMUX_PKG_DEPENDS="gdbm, libbz2, libffi, liblzma, libsqlite, musl, ncurses, ncurses-ui-libs, openssl, readline"
 
 # The flag --with(out)-pymalloc (disable/enable specialized mallocs) is enabled by default and causes m suffix versions of python.
 # Set ac_cv_func_wcsftime=no to avoid errors such as "character U+ca0025 is not in range [U+0000; U+10ffff]"
