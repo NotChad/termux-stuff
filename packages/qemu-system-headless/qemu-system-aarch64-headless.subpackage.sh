c629fc233172609cc7238b5506123582d83df973
Leonid Plyushch <leonid.plyushch@gmail.com>
2018-12-24 22:13:33 +0200

    qemu-system-headless: fix dependencies: libncurses => ncurses, liblzo2 => liblzo

--- packages/qemu-system-headless/qemu-system-aarch64-headless.subpackage.sh
+++ packages/qemu-system-headless/qemu-system-aarch64-headless.subpackage.sh
@@ -3,5 +3,5 @@ bin/qemu-system-aarch64
 share/man/man1/qemu-system-aarch64.1
 "
 
-TERMUX_SUBPKG_DEPENDS="glib, libbz2, libgcc, liblzo2, libncurses, libpixman, libstdc++, musl, zlib"
+TERMUX_SUBPKG_DEPENDS="glib, libbz2, libgcc, liblzo, ncurses, libpixman, libstdc++, musl, zlib"
 TERMUX_SUBPKG_DESCRIPTION="A generic and open source machine emulator (aarch64)"
