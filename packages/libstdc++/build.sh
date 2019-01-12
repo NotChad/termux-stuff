194cd7962939c1554fa97b0bfc97fb3a6570c0a5
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-01-12 15:02:11 +0200

    packages/libstdc++: update to v8.2.0 and provide development files

--- packages/libstdc++/build.sh
+++ packages/libstdc++/build.sh
@@ -1,11 +1,29 @@
 TERMUX_PKG_HOMEPAGE=http://gcc.gnu.org
 TERMUX_PKG_DESCRIPTION="The standard C++ library"
-TERMUX_PKG_VERSION=6.4.0
+TERMUX_PKG_VERSION=8.2.0
 TERMUX_PKG_DEPENDS="libgcc, musl"
+TERMUX_PKG_KEEP_STATIC_LIBRARIES=true
 
-TERMUX_PKG_RM_AFTER_INSTALL="lib/libstdc++.so.6.0.22-gdb.py"
+TERMUX_PKG_INCLUDE_IN_DEVPACKAGE="
+lib/libstdc++.a
+lib/libstdc++fs.a
+lib/libsupc++.a
+lib/libstdc++.so
+lib/libstdc++.so.6.0.25-gdb.py
+"
 
 termux_step_make_install() {
-    cp -f "/opt/termux/toolchain-$TERMUX_ARCH/$TERMUX_HOST_PLATFORM/lib/"libstdc++.so.* \
-        "$TERMUX_PREFIX/lib/"
+	local SYSROOT=$TERMUX_STANDALONE_TOOLCHAIN/$TERMUX_HOST_PLATFORM
+
+	install -Dm700 "$SYSROOT/lib/libstdc++.so.6.0.25" "$TERMUX_PREFIX/lib/libstdc++.so.6.0.25"
+	ln -sfr "$TERMUX_PREFIX/lib/libstdc++.so.6.0.25" "$TERMUX_PREFIX/lib/libstdc++.so.6"
+	ln -sfr "$TERMUX_PREFIX/lib/libstdc++.so.6.0.25" "$TERMUX_PREFIX/lib/libstdc++.so"
+
+	for lib in libstdc++.a libstdc++fs.a libsupc++.a; do
+		install -Dm600 "$SYSROOT/lib/$lib" "$TERMUX_PREFIX/lib/$lib"
+		$STRIP --strip-unneeded "$TERMUX_PREFIX/lib/$lib"
+	done
+
+	rm -rf "$TERMUX_PREFIX/include/c++"
+	cp -a "$SYSROOT/include/c++" "$TERMUX_PREFIX/include/"
 }
