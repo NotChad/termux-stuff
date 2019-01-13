73eb8a9b9a548d2cf78a57322526d6313676ad08
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-01-13 02:05:49 +0200

    packages/unzip: fix compilation error
    Use proper flags passed to make.

--- packages/unzip/build.sh
+++ packages/unzip/build.sh
@@ -6,10 +6,17 @@ TERMUX_PKG_SHA256=036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd
 TERMUX_PKG_DEPENDS="libbz2, musl"
 TERMUX_PKG_BUILD_IN_SRC=yes
 
-termux_step_configure () {
-	cp unix/Makefile Makefile
+termux_step_make() {
+	DEFINES='-DACORN_FTYPE_NFS -DWILD_STOP_AT_DIR -DLARGE_FILE_SUPPORT \
+			-DUNICODE_SUPPORT -DUNICODE_WCHAR -DUTF8_MAYBE_NATIVE -DNO_LCHMOD \
+			-DDATE_FORMAT=DF_YMD -DUSE_BZIP2 -DNOMEMCPY -DNO_WORKING_ISPRINT'
+
+	make -f unix/Makefile prefix=$TERMUX_PREFIX \
+			D_USE_BZ2=-DUSE_BZIP2 L_BZ2=-lbz2 \
+			CC="$CC" LD="$CC" LF2="$LDFLAGS" CF="$CFLAGS $CPPFLAGS -I. $DEFINES" \
+			unzips
 }
 
-termux_step_make () {
-	LD="$CC $LDFLAGS -lbz2" CC="$CC $CFLAGS $CPPFLAGS $LDFLAGS -DNO_LCHMOD -DUSE_BZIP2" make -j $TERMUX_MAKE_PROCESSES generic
+termux_step_make_install() {
+	make -f unix/Makefile prefix="$TERMUX_PREFIX" install
 }
