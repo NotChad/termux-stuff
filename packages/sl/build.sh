1ee44f7932746896579a557d0994c3de67c5f15c
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-01-13 02:21:06 +0200

    new packages: hexcurse, sl, stunnel, ttyrec, wcalc, yasm

---
+++ packages/sl/build.sh
@@ -0,0 +1,12 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/mtoyoda/sl
+TERMUX_PKG_DESCRIPTION="Tool curing your bad habit of mistyping"
+TERMUX_PKG_VERSION=5.02
+TERMUX_PKG_SRCURL=https://github.com/mtoyoda/sl/archive/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=1e5996757f879c81f202a18ad8e982195cf51c41727d3fea4af01fdcbbb5563a
+TERMUX_PKG_DEPENDS="musl, ncurses"
+TERMUX_PKG_BUILD_IN_SRC=yes
+
+termux_step_make_install() {
+	install sl $TERMUX_PREFIX/bin/
+	cp sl.1 $TERMUX_PREFIX/share/man/man1
+}
